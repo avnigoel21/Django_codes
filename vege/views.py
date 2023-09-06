d0ac79ae75546fb9b7f5a03aee8dfc9ebfbf0568
Avni Goel <avnigoel98@gmail.com>
2023-09-06 19:43:21 +0530

    working on vege

--- vege/views.py
+++ vege/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 
+
+from .models import *
 # Create your views here.
 
 
@@ -11,8 +13,14 @@ def receipes(request):
         receipe_name = data.get('receipe_name')
         receipe_description = data.get('receipe_description')
 
-        print(receipe_name)
-        print(receipe_description)
+        # print(receipe_name)
+        # print(receipe_description)
+        
+        Receipe.objects.create(
+            receipe_name = receipe_name,
+            receipe_description = receipe_description,
+        )
+
 
 
     return render (request , 'receipes/receipes.html' )
