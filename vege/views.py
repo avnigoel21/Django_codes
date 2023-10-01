886f18dfda1ce9368bac008d7e7f59b7865de8ac
Avni Goel <avnigoel98@gmail.com>
2023-10-01 20:53:17 +0530

    working on update receipe

--- vege/views.py
+++ vege/views.py
@@ -30,9 +30,32 @@ def receipes(request):
 
 
 def delete_receipes(request, id):
-    return redirect('receipes/')
+
+    queryset = Recipe.objects.get(id = id)
+    queryset.delete()
+    return redirect('/receipes')
 
 def update_receipes(request, id):
-    return redirect('receipes/')
+
+    queryset = Recipe.objects.get(id = id)
+
+    if request.method == "POST":
+        data = request.POST
+        receipe_name = data.get('receipe_name')
+        receipe_description = data.get('receipe_description')
+        receipe_image = request.FILES.get('receipe_image')
+
+        queryset.receipe_name = receipe_name
+        queryset.receipe_description = receipe_description
+
+        if receipe_image :
+            queryset.receipe_image = receipe_image
+
+        queryset.save()
+        return redirect('/receipes')
+    
+    context = {'receipe' : queryset}
+
+    return render (request , 'receipes/update_receipes.html' , context )
 
 
