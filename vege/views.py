55e4457431d3dd754a61f3c8335f30f761171195
Avni Goel <avnigoel98@gmail.com>
2023-10-15 20:03:05 +0530

    login, register completed

--- vege/views.py
+++ vege/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render , redirect
 from .models import *
+
+from django.contrib.auth.models import User
+
+from django.contrib import messages
+
+from django.contrib.auth import authenticate, login
+
+
 # Create your views here.
 
 def receipes(request):
@@ -67,10 +75,54 @@ def update_receipes(request, id):
 
 
 def login_page(request):
+
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+
+        if not User.objects.filter(username = username).exists():
+            messages.error(request, "Invalid Username")
+            return redirect("/login/")
+        
+        user = authenticate(username = username , password = password)
+
+        if user is None:
+            messages.error(request, "Invald Password")
+            return redirect("/login/")
+        else:
+            login(request, user)
+            return redirect("/receipes")
+
     return render(request, 'receipes/login.html')
 
 
 def register(request):
+
+    if request.method == "POST":
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = User.objects.filter(username = username)
+
+        if user.exists():
+            messages.info(request, "Username already taken") 
+            return redirect('/register/')
+
+        user = User.objects.create(
+            first_name = first_name,
+            last_name = last_name,
+            username = username
+        )
+
+        user.set_password(password)
+        user.save()
+
+        messages.info(request, "Account created successfully")
+
+        return redirect('/register/')
+
     return render(request, 'receipes/register.html')
 
 
