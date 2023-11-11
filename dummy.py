21acbbbc0028b5c318b4f75e6fcfc4778b0a3d7e
Avni Goel <avnigoel98@gmail.com>
2023-11-11 21:39:29 +0530

    working on Student_report_card database

---
+++ dummy.py
@@ -0,0 +1,14 @@
+# number = 2
+# name = "avni"
+# print("student id is" , number)
+
+# print(f"student {name}{number} id is {number}")
+
+# # STU-0001
+# print(f"STU-{number}")
+
+#          0          1              2                 3
+list = ["civil" , "mechanical" , "computer science" , "IT"]
+
+print(list)
+print(list[3])
